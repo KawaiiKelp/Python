8233a2c605ac110cfe8d0250b3c31561e12db929
KawaiiKelp <plekcontact12@gmail.com>
2025-09-01 10:50:49 +0900

    asdf and index

---
+++ index.py
@@ -0,0 +1,16 @@
+# 문자열 인덱싱과 슬라이싱
+
+a = "Life is too short, You need Python"
+print(len(a))
+
+print(a[5]) # i
+print(a[19]) # Y
+print(a[0]+a[19] + a[28])
+print(a[12:17])
+print(a[23:27])
+print(a[8:17])
+print(a[:17])
+print(a[:])
+print(a[19:])
+print(a[19:-7])
+print(a[:-9])
